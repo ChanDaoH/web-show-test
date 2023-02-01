cac082ab0acde0831e7de49fd075c9bb67a740f1
泽尘 <zechen.hg@alibaba-inc.com>
2023-02-01 21:19:27 +0800

    add stress func

--- stressfunc/index.js
+++ stressfunc/index.js
@@ -3,12 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('*', (req, res) => {
-    res.header('Content-Type', 'text/html;charset=utf-8')
-    console.log('test');
-
-    setTimeout(() => {
-      res.send(`hello world`)
-    }, 500);
+    res.send(`hello world`)
 })
 
 app.listen(9000, () => {
